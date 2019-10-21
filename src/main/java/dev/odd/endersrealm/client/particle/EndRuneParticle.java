ea45402b58058cecda8fc263fdfd2f26f5cb43b9
Nick Kennedy <nickkennedy334@gmail.com>
2019-10-20 20:17:46 -0400

    ARR Because I Don't Know What License to actually use.

--- src/main/java/dev/odd/endersrealm/client/particle/EndRuneParticle.java
+++ src/main/java/dev/odd/endersrealm/client/particle/EndRuneParticle.java
@@ -14,34 +14,35 @@ import net.minecraft.world.World;
 @Environment(EnvType.CLIENT)
 public class EndRuneParticle extends SpriteBillboardParticle {
 
-    protected EndRuneParticle(World world, double xPos, double yPos, double zPos, double xVelocity, double yVelocity, double zVelocity) {
+    protected EndRuneParticle(World world, double xPos, double yPos, double zPos, double xVelocity, double yVelocity,
+            double zVelocity) {
         super(world, xPos, yPos, zPos);
         this.setBoundingBoxSpacing(0.02F, 0.02F);
         this.scale *= this.random.nextFloat() * 0.6F + 0.2F;
         this.velocityX = velocityX * 0.20000000298023224D + (Math.random() * 2.0D - 1.0D) * 0.019999999552965164D;
         this.velocityY = velocityY * 0.20000000298023224D + (Math.random() * 2.0D - 1.0D) * 0.019999999552965164D;
         this.velocityZ = velocityZ * 0.20000000298023224D + (Math.random() * 2.0D - 1.0D) * 0.019999999552965164D;
-        this.maxAge = (int)(8.0D / (Math.random() * 0.8D + 0.2D));
+        this.maxAge = (int) (8.0D / (Math.random() * 0.8D + 0.2D));
     }
 
     @Override
     public void tick() {
-      this.prevPosX = this.x;
-      this.prevPosY = this.y;
-      this.prevPosZ = this.z;
-      if (this.maxAge-- <= 0) {
-         this.markDead();
-      } else {
-         this.velocityY += 0.002D;
-         this.move(this.velocityX, this.velocityY, this.velocityZ);
-         this.velocityX *= 0.8500000238418579D;
-         this.velocityY *= 0.8500000238418579D;
-         this.velocityZ *= 0.8500000238418579D;
-         if (!this.world.isAir(new BlockPos(this.x, this.y, this.z))) {
+        this.prevPosX = this.x;
+        this.prevPosY = this.y;
+        this.prevPosZ = this.z;
+        if (this.maxAge-- <= 0) {
             this.markDead();
-         }
+        } else {
+            this.velocityY += 0.002D;
+            this.move(this.velocityX, this.velocityY, this.velocityZ);
+            this.velocityX *= 0.8500000238418579D;
+            this.velocityY *= 0.8500000238418579D;
+            this.velocityZ *= 0.8500000238418579D;
+            if (!this.world.isAir(new BlockPos(this.x, this.y, this.z))) {
+                this.markDead();
+            }
 
-      }
+        }
     }
 
     @Override
@@ -57,16 +58,13 @@ public class EndRuneParticle extends SpriteBillboardParticle {
             this.sprite = spriteProvider_1;
         }
 
-        public Particle method_3012(DefaultParticleType type, World world, double xPos, double yPos, double zPos, double xVelocity, double yVelocity, double zVelocity) {
-            EndRuneParticle endRuneParticle = new EndRuneParticle(world, xPos, yPos, zPos, xVelocity, yVelocity, zVelocity);
-            endRuneParticle.setSprite(this.sprite);
-            return endRuneParticle;
-        }
-
         @Override
         public Particle createParticle(DefaultParticleType type, World world, double xPos, double yPos, double zPos,
                 double xVelocity, double yVelocity, double zVelocity) {
-                    return new EndRuneParticle(world, xPos, yPos, zPos, xVelocity, yVelocity, zVelocity);
+            EndRuneParticle endRuneParticle = new EndRuneParticle(world, xPos, yPos, zPos, xVelocity, yVelocity,
+                    zVelocity);
+            endRuneParticle.setSprite(this.sprite);
+            return endRuneParticle;
         }
     }
 }
